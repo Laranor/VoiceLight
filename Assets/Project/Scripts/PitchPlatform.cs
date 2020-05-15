4abda93790370347cd7ccd8053560824113552c3
Laranor <61905814+Laranor@users.noreply.github.com>
2020-05-15 22:33:42 +0200

    Test plateforme pitch

---
+++ Assets/Project/Scripts/PitchPlatform.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitchPlatform : MonoBehaviour
+{
+    public AvatarLighting avatar;
+
+    [SerializeField] private float pitchValue;
+    [SerializeField] private float dbValue;
+    public float maxHeight;
+    public float minHeight;
+    public float seuilPitch;
+    public float speed;
+
+    private void OnCollisionStay(Collision other)
+    {
+        pitchValue = avatar.PitchValue;
+        dbValue = avatar.DbValue;
+        speed = (dbValue + 80) / 40;
+        if (other.gameObject.name == "Avatar")
+        {
+            if(pitchValue > seuilPitch && pitchValue > 0)
+            {
+                if (transform.position.y < maxHeight)
+                {
+                    transform.position += new Vector3(0, 1+speed, 0) * Time.deltaTime;
+                }
+            }
+            if (pitchValue <= seuilPitch && pitchValue > 0)
+            {
+                if (transform.position.y > minHeight )
+                {
+                    transform.position -= new Vector3(0, 1+speed, 0) * Time.deltaTime;
+                }
+            }
+        }
+    }
+}
