0b8a7f9aaff0be0233ec288c4e4e4c7cada6437b
Laranor <61905814+Laranor@users.noreply.github.com>
2020-05-21 04:03:20 +0200

    minigolf début

---
+++ Assets/Project/Scripts/AtelierGolf/Waypoints.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    public Transform[] wayPointList;
+
+    public int currentWayPoint = 0;
+    Transform targetWayPoint;
+
+    public AvatarLighting avatarScript;
+    public GameObject avatar;
+
+    [SerializeField] private float pitchValue;
+    [SerializeField] private float dbValue;
+
+    private float speed;
+    public float diviseur;
+    public bool open;
+
+    public float distanceToActivate;
+    void Update()
+    {
+        if ((avatar.transform.position - transform.position).magnitude < distanceToActivate)
+        {
+            pitchValue = avatarScript.PitchValue;
+            dbValue = avatarScript.DbValue;
+            speed = (dbValue + 80) / diviseur;
+            if (currentWayPoint < this.wayPointList.Length)
+            {
+                if (targetWayPoint == null)
+                    targetWayPoint = wayPointList[currentWayPoint];
+                walk();
+            }
+        }
+        if(currentWayPoint == this.wayPointList.Length)
+        {
+            open = true;
+        }
+    }
+
+    void walk()
+    {
+        // rotate towards the target
+        transform.forward = Vector3.RotateTowards(transform.forward, targetWayPoint.position - transform.position, speed * Time.deltaTime, 0.0f);
+
+        // move towards the target
+        transform.position = Vector3.MoveTowards(transform.position, targetWayPoint.position, speed * Time.deltaTime);
+
+        if (transform.position == targetWayPoint.position)
+        {
+            currentWayPoint++;
+            targetWayPoint = wayPointList[currentWayPoint];
+        }
+    }
+}
